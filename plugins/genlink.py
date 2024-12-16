506c58be06f2f7713e3e4280d6ab3b6b01fabb31
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2024-12-16 16:35:20 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -31,8 +31,12 @@ async def gen_link_s(bot, message):
     string = 'filep_' if message.text.lower().strip() == "/plink" else 'file_'
     string += file_id
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"Here is your Link:\nhttps://t.me/{temp.U_NAME}?start={outstr}")    
     
+    if WEBSITE_URL_MODE == True:
+        await message.reply(f"Here is your Link:\n{WEBSITE_URL}?Tech_VJ={outstr}")
+    else:
+        await message.reply(f"Here is your Link:\nhttps://t.me/{temp.U_NAME}?start={outstr}")  
+     
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
